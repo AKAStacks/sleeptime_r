3fa15af6b340eb7968cb57d34a306a0c285aadb4
AKAStacks <denniszarger@gmail.com>
2020-03-11 14:22:57 -0500

    Refactored, dialog now; need to move Dialog to its own function
    and handle the response in main()

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ extern crate gio;
 use gtk::prelude::*;
 use gio::prelude::*;
 
-use gtk::{Application, ApplicationWindow, Button, Entry, Box};
+use std::{thread, time};
+use gtk::{Application, Button, SpinButton, Dialog};
 
 fn main() {
     let application = Application::new(  // Prototype gtk's Application type
@@ -13,26 +14,53 @@ fn main() {
     ).expect("failed to initialize GTK application");
 
     application.connect_activate(|app| {
-        let window = ApplicationWindow::new(app);
-        window.set_title("First GTK+ Program");
-        window.set_default_size(350,70);
+        app.hold();
+        let dialog = Dialog::new();
+        let dialog_content_area = dialog.get_content_area();
 
-        let vbox = Box::new(gtk::Orientation::Vertical, 2);
+        let spinbutton = SpinButton::new_with_range(
+            0.0,    // min
+            480.0,  // max
+            10.0);  // step
+        let spinbutton_clone = spinbutton.clone();
 
-        let entry = Entry::new();
-        let entry_clone = entry.clone();
+        let settiimerbutton = Button::new_with_mnemonic(
+            "_Set Timer");
 
-        let button = Button::new_with_label("Click me!");
-        button.connect_clicked(move |_| {
-            let entry_val = entry_clone.get_buffer().get_text();
-            println!("{}", entry_val);
+        let cancelbutton = Button:: new_with_mnemonic(
+            "_Cancel")
+
+        settiimerbutton.connect_clicked(move |_| {
+            let sleepmins: f64 = spinbutton_clone.get_value();
+            //application_clone.hold();
+            dialog.close();
+            sleep_for(sleepmins);
+            println!("Slept for {} mins!", sleepmins);
+            app.release();
         });
 
-        vbox.add(&button);
-        vbox.add(&entry);
-        window.add(&vbox);
-        window.show_all();
+        dialog_content_area.add(&spinbutton);
+        dialog.add_action_widget(&settiimerbutton, gtk::ResponseType::Accept);
+        dialog.add_action_widget(&cancelbutton, gtk::ResponseType::Cancel);
+        dialog.show_all();
+        dialog.run();
+        //window.add(&vbox);
+        //window.show_all();
     });
 
     application.run(&[]);
+}
+
+fn show_input_dialog(app: Application) {
+}
+
+fn sleep_for(minutes: f64) {
+    let secondsconv = minutes * 60.0;
+    let secondsint = secondsconv as i64;
+    let onesecond = time::Duration::new(1,0);
+    println!("Sleeping for {} seconds:", secondsint.to_string());
+    for second in 0..secondsint {
+        println!("{}", second.to_string());
+        thread::sleep(onesecond);
+    }
 }
